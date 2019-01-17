b01cf8fca2b02c9f7dc921ba2ff8f479c33ff2bf
Muh.Isfahani Ghiyath.YM <isfaaghyth@users.noreply.github.com>
2019-01-17 07:57:23 +0700

    change to kotlin lang

---
+++ app/src/main/java/isfaaghyth/app/dashis/network/Network.kt
@@ -0,0 +1,8 @@
+package isfaaghyth.app.dashis.network
+
+/**
+ * Created by isfaaghyth on 17/01/19.
+ * github: @isfaaghyth
+ */
+object Network {
+}
---
+++ app/src/main/java/isfaaghyth/app/dashis/ui/login/LoginActivity.kt
@@ -0,0 +1,14 @@
+package isfaaghyth.app.dashis.ui.login
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+import isfaaghyth.app.dashis.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
